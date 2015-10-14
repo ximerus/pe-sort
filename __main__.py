5b716fb267339f440bb5e3b98a6d7bc0ecb1d6e2
ximerus <ximerus@gmail.com>
2015-10-15 01:37:16 +0300

    Add print sections

--- __main__.py
+++ __main__.py
@@ -2,42 +2,55 @@
 
 import pefile
 import sys
-import time
 import os
 
 
 def main():
-    files_eq = 0
-    files_neq = 0
-    total_files = 0
+
     path = sys.argv[1]
+
     if path == 0:
         usage(sys.argv[0])
         exit()
+
     files_count = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])
-    sys.stdout.write("Total files in dir: "+str(files_count))
+    sys.stdout.write("Total files in dir: %s\n" % str(files_count))
     i = 0
-    percent = 0
     for pe_file in os.listdir(path):
         if os.path.isfile(os.path.join(path, pe_file)):
-            if i == (files_count / 100):
-                i = 0
-                percent += 1
-                time.sleep(1)
-                sys.stdout.write(("\rAnalysing in process: ["+("="*percent)+"] %d"%percent+"%"))
-                sys.stdout.flush()
-            i += 1
+            if is_pe_file(os.path.join(path, pe_file)):
+                sys.stdout.write("\nFile: " + pe_file + "\n")
+                print_sections(os.path.join(path, pe_file))
+                i += 1
+
+
+def print_sections(in_file):
+    sample = pefile.PE(in_file)
+
+    for section in sample.sections:
+        sys.stdout.write("Name: %0s" % section.Name + "\tRawSize = 0x%08x" % section.SizeOfRawData)
+        sys.stdout.write("\tVirtualSize = 0x%08x" % section.Misc_VirtualSize + "\n")
+
+
+def is_pe_file(in_file):
+    readed_file = open(in_file, 'r')
+    if readed_file.read(2) == 'MZ':
+        return 1
+    else:
+        return 0
+
 
 
 def print_msg(code, msg):
     if code == -1:
-        print "[ERROR]\t"+msg
+        print "[ERROR]\t" + msg
     else:
-        print "[INFO]\t"+msg
+        print "[INFO]\t" + msg
 
 
 def usage(module_name):
-    print 'Usage: '+module_name+" <folder_with_pe_files>"
+    print 'Usage: ' + module_name + " <folder_with_pe_files>"
+
 
 if __name__ == '__main__':
     main()
